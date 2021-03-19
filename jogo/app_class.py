85f4068e5b564251c5de96f144a63c53ad11b734
gapigo <x.gabriel12@gmail.com>
2021-03-19 00:50:19 -0300

    Sudoku jogo parte 2

--- jogo/app_class.py
+++ jogo/app_class.py
@@ -12,14 +12,27 @@ class App:
         # Seta o estado de 'rodando' ao programa
         self.running = True
 
+        # Guarda a matriz do tabuleiro de sudoku
         self.grid = testBoard
+
+        # Armazena a informação do botão selecionado atualmente
+        self.selected = None
+
+        # Rastreia a posição do mouse
+        self.mousePos = None
 
     def run(self):
 
         # Enquanto o programa estiver rodando, executar os métodos:
         while self.running:
+
+            # Função que detecta os eventos do jogo
             self.events()
+
+            # Função que realiza os updates do jogo
             self.update()
+
+            # Função que se responsabiliza por desenhar a GUI
             self.draw()
 
         # Quando sair do while, chama os métodos de saída
@@ -33,19 +46,39 @@ class App:
             if event.type == pygame.QUIT:
                 self.running = False
 
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                selected = self.mouseOnGrid()
+
+                # selected pode ser False ou uma tupla com a casa do jogo que foi selecionada
+                if selected:
+                    self.selected = selected
+                else:
+                    print("Not on board")
+                    self.selected = None
+
     def update(self):
-        pass
+        # Atualiza a posição do mouse
+        self.mousePos = pygame.mouse.get_pos()
 
     def draw(self):
 
         # Seta a cor a ser preenchida como branca
         self.window.fill(WHITE)
 
+        # Desenha a casa selecionada
+        if self.selected:
+            self.drawSelection(self.window, self.selected)
+
         # Desenha a grade (grid) do sudoku enviando o "palco" self.window como parâmetro
         self.drawGrid(self.window)
 
         # Atualiza o display
         pygame.display.update()
+
+    def drawSelection(self, window, pos):
+        # Desenha um retângulo com os seguintes parâmetros:
+        # (A janela onde vai desenhar, a cor do retângulo, a posição (left, top, tamanho pra direita, tamanho pra esquerda))
+        pygame.draw.rect(window, LIGHTBLUE, ((pos[0]*cellSize) + gridPos[0], (pos[1]*cellSize) + gridPos[1], cellSize, cellSize))
 
     def drawGrid(self, window):
 
@@ -76,9 +109,21 @@ class App:
                 # Repete o comando da linha, engrossando 2px
                 pygame.draw.line(window, BLACK, (gridPos[0], gridPos[1] + (x * cellSize)), (gridPos[0] + 450, gridPos[1] + (x * cellSize)), 2)
 
+    '''
+    Comentário da função mouseOnGrid():
+        Se o mouse estiver fora dentro do tabuleiro, ele retornará false,
+        mas, se ele estiver dentro, ele retornará a casa que foi clicada.
+        Essa função é chamada toda vez que ocorre um evento de click.
+    '''
+    def mouseOnGrid(self):
+        # Confere se a posição do mouse está fora do tabuleiro pra menos
+        if self.mousePos[0] < gridPos[0] or self.mousePos[1] < gridPos[1]:
+            return False
 
-        # # ridx = row index
-        # Desenha as colunas
-        # # cidx = column index
-        # for ridx, row in enumerate(self.grid):
-        #     for cidx, cell in enumerate(self.row):
+        # Confere se a posição do mouse está fora do tabuleiro pra mais
+        if self.mousePos[0] > gridPos[0] + gridSize or self.mousePos[1] > gridPos[1] + gridSize:
+            return False
+
+        # Se a função chegou até aqui, é pq a posição do mouse está dentro do tabuleiro, então:
+        return ((self.mousePos[0]-gridPos[0])//cellSize, (self.mousePos[1]-gridPos[1])//cellSize)
+
--- jogo/settings.py
+++ jogo/settings.py
@@ -4,11 +4,13 @@ HEIGHT = 600
 # Cores definidas (tupla no padrão RGB)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+LIGHTBLUE = (96, 216, 232)
 
 # Boards (tabuleiros)
 testBoard = [[0 for x in range(9)] for x in range(9)]   # forma uma matriz 9x9 com '0s'
 
 # Posição e tamanho da grade (Positions and sizes)
 gridPos = (75, 100)  # grid Position (left, top)px from border
-cellSize = 50
+cellSize = 50  # tamanho de uma cada do sudoku (1 quadradinho dos 9x9)
+gridSize = cellSize * 9  # tamanho do tabuleiro de sudoku
 
