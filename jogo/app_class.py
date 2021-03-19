57a2e63019186007d804d1c74dfd4914bcaaf3b6
gapigo <x.gabriel12@gmail.com>
2021-03-18 23:58:55 -0300

    Sudoku jogo parte 1

--- jogo/app_class.py
+++ jogo/app_class.py
@@ -12,6 +12,8 @@ class App:
         # Seta o estado de 'rodando' ao programa
         self.running = True
 
+        self.grid = testBoard
+
     def run(self):
 
         # Enquanto o programa estiver rodando, executar os métodos:
@@ -24,7 +26,6 @@ class App:
         pygame.quit()
         sys.exit()
 
-
     def events(self):
         for event in pygame.event.get():
 
@@ -36,5 +37,48 @@ class App:
         pass
 
     def draw(self):
+
+        # Seta a cor a ser preenchida como branca
         self.window.fill(WHITE)
+
+        # Desenha a grade (grid) do sudoku enviando o "palco" self.window como parâmetro
+        self.drawGrid(self.window)
+
+        # Atualiza o display
         pygame.display.update()
+
+    def drawGrid(self, window):
+
+        # Desenha um retângulo com os seguintes parâmetros:
+        # (A janela onde vai desenhar, a cor do retângulo, a posição (left, top, tamanho pra direita, tamanho pra baixo), o tamanho da borda)
+        pygame.draw.rect(window, BLACK, (gridPos[0], gridPos[1], WIDTH - 150, HEIGHT - 150), 2)
+
+        for x in range(9):
+
+            # Para que a cada três linhas o programa desenhe uma linha mais grossa
+            if x % 3 != 0:
+                '''Desenha a coluna'''
+                # Desenha uma linha com os seguintes parâmetros:
+                # (A janela onde vai desenhar, a cor da linha, a posição do ponto inicial, a posição do ponto final)
+                # (gridPos[0] + (x* cellSize)) se refere a posição x (horizontal) na janela, gridPos à borda esquerda do tabuleiro e cellSize o tamanho da casa
+                # (gridPos[1]) se refere a posição y (vertical) na janela. Então temos (x, y) para definir a posição do ponto.
+                pygame.draw.line(window, BLACK, (gridPos[0] + (x * cellSize), gridPos[1]), (gridPos[0] + (x * cellSize), gridPos[1] + 450))
+
+                '''Desenha a linha'''
+                # Faz a mesma coisa que o comando anterior, mas adaptando pra que seja uma linha horizontal
+                pygame.draw.line(window, BLACK, (gridPos[0], gridPos[1] + (x * cellSize)), (gridPos[0] + 450, gridPos[1] + (x * cellSize)))
+            else:
+                '''Desenha a coluna'''
+                # Faz o mesmo que o comando anterior, mas no final o parâmetro 2 engrossa a linha para 2px, em vez do padrão 1px
+                pygame.draw.line(window, BLACK, (gridPos[0] + (x * cellSize), gridPos[1]), (gridPos[0] + (x * cellSize), gridPos[1] + 450), 2)
+
+                '''Desenha a linha'''
+                # Repete o comando da linha, engrossando 2px
+                pygame.draw.line(window, BLACK, (gridPos[0], gridPos[1] + (x * cellSize)), (gridPos[0] + 450, gridPos[1] + (x * cellSize)), 2)
+
+
+        # # ridx = row index
+        # Desenha as colunas
+        # # cidx = column index
+        # for ridx, row in enumerate(self.grid):
+        #     for cidx, cell in enumerate(self.row):
--- jogo/settings.py
+++ jogo/settings.py
@@ -3,3 +3,12 @@ HEIGHT = 600
 
 # Cores definidas (tupla no padrão RGB)
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# Boards (tabuleiros)
+testBoard = [[0 for x in range(9)] for x in range(9)]   # forma uma matriz 9x9 com '0s'
+
+# Posição e tamanho da grade (Positions and sizes)
+gridPos = (75, 100)  # grid Position (left, top)px from border
+cellSize = 50
+
